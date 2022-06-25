9936218eb1ed3eae5de243ebdeca5ebae5c6d887
peti <95631000+darthpeti@users.noreply.github.com>
2022-06-25 18:17:27 +0300

    keepinv for siege participants only

--- src/main/java/me/darthpeti/townytweaks/Towny/listeners/KeepInvSiege.java
+++ src/main/java/me/darthpeti/townytweaks/Towny/listeners/KeepInvSiege.java
@@ -1,7 +1,11 @@
 package me.darthpeti.townytweaks.Towny.listeners;
 
+import com.gmail.goosius.siegewar.SiegeController;
+import com.gmail.goosius.siegewar.objects.Siege;
+import com.gmail.goosius.siegewar.utils.SiegeWarAllegianceUtil;
 import com.gmail.goosius.siegewar.utils.SiegeWarDistanceUtil;
-import me.darthpeti.townytweaks.Main;
+import com.palmergames.bukkit.towny.TownyUniverse;
+import com.palmergames.bukkit.towny.object.Resident;
 import me.darthpeti.townytweaks.Towny.util.ConfigUtil;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
@@ -12,12 +16,16 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 public class KeepInvSiege implements Listener {
     @EventHandler
     public void onDeath(PlayerDeathEvent event) {
-        if(ConfigUtil.keepInventoryInSiege()) {
+        if (ConfigUtil.keepInventoryInSiege()) {
             Location deathloc = event.getPlayer().getLocation();
             Player player = event.getPlayer();
-            if(player.hasPermission("siegewar.nation.siege.battle.points") || player.hasPermission("siegewar.town.siege.battle.points")){
+            Resident resident = TownyUniverse.getInstance().getResident(player.getName());
+            if (player.hasPermission("siegewar.nation.siege.battle.points") || player.hasPermission("siegewar.town.siege.battle.points")) {
                 if (SiegeWarDistanceUtil.isLocationInActiveSiegeZone(deathloc)) {
-                    event.setKeepInventory(true);
+                    Siege siege = SiegeController.getActiveSiegeAtLocation(deathloc);
+                    if (SiegeWarAllegianceUtil.isPlayerOnTownFriendlySide(player, resident, siege) || SiegeWarAllegianceUtil.isPlayerOnTownHostileSide(player, resident, siege)) {
+                        event.setKeepInventory(true);
+                    }
                 }
             }
         }
