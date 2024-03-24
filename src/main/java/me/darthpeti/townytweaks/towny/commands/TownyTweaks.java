be190206d1a17bfcb5e8b440c54ceb9308501bc8
Aomsin <me@masuru.in.th>
2024-03-24 23:48:27 +0700

    feat: update version to 0.1.5

--- src/main/java/me/darthpeti/townytweaks/towny/commands/TownyTweaks.java
+++ src/main/java/me/darthpeti/townytweaks/towny/commands/TownyTweaks.java
@@ -11,39 +11,36 @@ public class TownyTweaks implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if(args.length > 0){
-            if(args[0].equalsIgnoreCase("reload")){
-                Main.getInstance().reloadCustomConfig();
-                if(sender instanceof Player){
-                    sender.sendMessage(Main.prefix + "Configuration Reloaded., if no changes are visible, please restart the server or use plugman to reload the plugin.");
-                    return true;
-                } else {
-                    Main.log.info("Configuration Reloaded., if no changes are visible, please restart the server or use plugman to reload the plugin.");
-                    return true;
-                }
-            }
-
-            if(args[0].equalsIgnoreCase("version")){
-                if(sender instanceof Player){
-                    sender.sendMessage(Main.prefix + "TownyTweaks Version: " + Main.getInstance().getDescription().getVersion());
-                    return true;
-                } else {
-                    Main.log.info("TownyTweaks Version: " + Main.getInstance().getDescription().getVersion());
-                    return true;
-                }
-            }
-
-            if (args[0].equalsIgnoreCase("help")) {
-                if (sender instanceof Player) {
-                    sender.sendMessage(Main.prefix + "TownyTweaks Help:");
-                    sender.sendMessage(Main.prefix + "/townytweaks reload - Reloads the configuration file.");
-                    sender.sendMessage(Main.prefix + "/townytweaks version - Shows the version of the plugin.");
-                    return true;
-                } else {
-                    Main.log.info("TownyTweaks Help:");
-                    Main.log.info("/townytweaks reload - Reloads the configuration file.");
-                    Main.log.info("/townytweaks version - Shows the version of the plugin.");
-                    return true;
-                }
+            switch (args[0].toLowerCase()) {
+                case "reload":
+                    Main.getInstance().reloadCustomConfig();
+                    if(sender instanceof Player){
+                        sender.sendMessage(Main.prefix + "Configuration Reloaded., if no changes are visible, please restart the server or use plugman to reload the plugin.");
+                        return true;
+                    } else {
+                        Main.log.info("Configuration Reloaded., if no changes are visible, please restart the server or use plugman to reload the plugin.");
+                        return true;
+                    }
+                case "version":
+                    if(sender instanceof Player){
+                        sender.sendMessage(Main.prefix + "TownyTweaks Version: " + Main.getInstance().getDescription().getVersion() + " by DarthPeti, rework by: @Aomkoyo");
+                        return true;
+                    } else {
+                        Main.log.info("TownyTweaks Version: " + Main.getInstance().getDescription().getVersion()+ " by DarthPeti, rework by: @Aomkoyo");
+                        return true;
+                    }
+                default:
+                    if (sender instanceof Player) {
+                        sender.sendMessage(Main.prefix + "TownyTweaks Help:");
+                        sender.sendMessage(Main.prefix + "/townytweaks reload - Reloads the configuration file.");
+                        sender.sendMessage(Main.prefix + "/townytweaks version - Shows the version of the plugin.");
+                        return true;
+                    } else {
+                        Main.log.info("TownyTweaks Help:");
+                        Main.log.info("/townytweaks reload - Reloads the configuration file.");
+                        Main.log.info("/townytweaks version - Shows the version of the plugin.");
+                        return true;
+                    }
             }
         }
         return false;
