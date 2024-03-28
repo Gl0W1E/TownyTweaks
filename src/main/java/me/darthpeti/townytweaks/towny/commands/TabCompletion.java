0a4b59cf826e78042723210c348f29275509f382
Aomsin <me@masuru.in.th>
2024-03-28 13:14:43 +0700

    feat: TabCompletion

---
+++ src/main/java/me/darthpeti/townytweaks/towny/commands/TabCompletion.java
@@ -0,0 +1,17 @@
+package me.darthpeti.townytweaks.towny.commands;
+
+import java.util.List;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+public class TabCompletion implements TabCompleter {
+    @Override
+    public List<String> onTabComplete (CommandSender sender, Command cmd, String label, String[] args){
+        if(args.length == 1){
+            return List.of("reload", "version");
+        }
+        return null;
+    }
+}
